b0de00cda877d06fb8517d2e29a71dcafcb6ef44
jonathancross1013 <140754479+jonathancross1013@users.noreply.github.com>
2023-11-10 03:35:07 -0500

    dark / light transition buttons / UI overhaul / header changer functions

--- script.js
+++ script.js
@@ -27,6 +27,20 @@ function populateSubcategorySelect() {
     });
 }
 
+function sendCategoriesToExtension() {
+    // Assuming 'category-container' is the ID of the element where categories are managed
+    var categoryElement = document.getElementById('category-container');
+    if (categoryElement) {
+        try {
+            var categories = []; // Logic to fetch categories from local storage
+            window.postMessage({ type: "SEND_CATEGORIES", categories: categories }, "*");
+        } catch (error) {
+            console.error("Error sending categories to extension:", error);
+        }
+    }
+}
+
+
 function toggleSubTab(subtabName) {
     var subtab = document.getElementById(subtabName);
     if (subtab.style.display === 'none' || subtab.style.display === '') {
@@ -102,7 +116,18 @@ function createNewTab(categoryName) {
     tabContainer.onclick = function () {
         openTab(event, categoryName.toLowerCase().replace(/\s+/g, ''));
         openSubTab(event, categoryName.toLowerCase().replace(/\s+/g, ''));
-    };
+
+    chrome.storage.sync.get({categories: []}, function(data) {
+        let categories = data.categories;
+        if(categories.indexOf(categoryName) === -1) { // Avoid duplicates
+            categories.push(categoryName);
+            chrome.storage.sync.set({categories: categories}, function() {
+                console.log('Categories updated with:', categoryName);
+            });
+        }
+    });
+
+};
 
     var removeButton = document.createElement("button");
     removeButton.textContent = "Remove Category";
@@ -238,17 +263,65 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     }
 });
-// Function to toggle dark mode
-function toggleDarkMode() {
-  var body = document.body;
-  body.classList.toggle('dark-mode');
 
-  if (body.classList.contains('dark-mode')) {
-    localStorage.setItem('darkMode', 'enabled');
-  } else {
-    localStorage.setItem('darkMode', 'disabled');
-  }
+function toggleDarkMode() {
+    var body = document.body;
+    var modeText = document.getElementById('dark-mode-text'); // Assuming you have a span with this id for the text
+    var moonIcon = document.querySelector('#dark-mode-toggle .fa-moon');
+    var sunIcon = document.querySelector('#dark-mode-toggle .fa-sun');
+
+    body.classList.toggle('dark-mode');
+
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('darkMode', 'enabled');
+        moonIcon.style.display = 'none';
+        sunIcon.style.display = 'inline-block';
+        modeText.textContent = 'Light Mode'; // Update text to Light Mode when dark mode is active
+    } else {
+        localStorage.setItem('darkMode', 'disabled');
+        moonIcon.style.display = 'inline-block';
+        sunIcon.style.display = 'none';
+        modeText.textContent = 'Dark Mode'; // Update text to Dark Mode when light mode is active
+    }
 }
+
+// Ensure to call toggleDarkMode to set the initial state when the page loads
+document.addEventListener('DOMContentLoaded', function() {
+    var darkModeButton = document.getElementById('dark-mode-toggle');
+    if (darkModeButton) {
+        darkModeButton.addEventListener('click', toggleDarkMode);
+    }
+
+    // Initialize the correct mode based on the saved setting
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        toggleDarkMode();
+    }
+});
+
+
+
+// Listen for messages from the extension
+window.addEventListener('message', function(event) {
+    // Make sure the message is from your extension
+    if (event.source == window && event.data.type && (event.data.type == "FROM_EXTENSION")) {
+        console.log("Data received from extension:", event.data);
+        // Now handle the data, for example, save it to local storage
+        const { referenceName, referenceContent, referenceCategory } = event.data;
+        saveReference(referenceName, referenceContent, referenceCategory);
+    }
+});
+
+function saveReference(name, content, category) {
+    // Retrieve the current array of references or initialize it if not present
+    var references = JSON.parse(localStorage.getItem('references')) || [];
+    // Create a new reference object
+    var newReference = { name: name, content: content, category: category };
+    // Add the new reference to the array
+    references.push(newReference);
+    // Save the updated array back to local storage
+    localStorage.setItem('references', JSON.stringify(references));
+}
+
 
 document.addEventListener('DOMContentLoaded', function() {
   var darkModeToggleButton = document.getElementById('dark-mode-toggle');
@@ -261,3 +334,33 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    var header = document.getElementById('header');
+    var colorPicker = document.getElementById('headerColorPicker');
+    var changeColorButton = document.getElementById('changeHeaderColorButton');
+    
+    // Function to update header color
+    function updateHeaderColor(color) {
+        header.style.backgroundColor = color;
+        if (!document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('headerColor', color); // Save color to localStorage
+        }
+    }
+
+    // Load saved color from localStorage and update the header if light mode is active
+    var savedColor = localStorage.getItem('headerColor');
+    if (savedColor && !document.body.classList.contains('dark-mode')) {
+        updateHeaderColor(savedColor);
+        colorPicker.value = savedColor; // Update color picker to show the saved color
+    }
+
+    // Event listener for the color picker
+    colorPicker.addEventListener('input', function() {
+        updateHeaderColor(colorPicker.value);
+    });
+
+    // Event listener for the button
+    changeColorButton.addEventListener('click', function() {
+        colorPicker.click(); // Trigger the color picker when the button is clicked
+    });
+});
